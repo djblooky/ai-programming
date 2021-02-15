a8045a0c420852216b1323cff32273b0ffcd0638
Charlie Phillips <hi.charliephillips@gmail.com>
2021-02-14 20:38:55 -0600

    cube jumps

--- Chapter2_MovementActivity/Assets/Scripts/Move.cs
+++ Chapter2_MovementActivity/Assets/Scripts/Move.cs
@@ -2,13 +2,19 @@
 
 public class Move : MonoBehaviour
 {
-    Vector3 goal;
-    float speed = 1.0f;
-    float accuracy = 1.0f;
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float accuracy = .5f;
+    [SerializeField] private float rotSpeed = 2f;
+    [SerializeField] private float jumpForce = 10f;
+
+    private Rigidbody rigidbody;
+    private Vector3 goal;
+    private bool jumped = false;
 
     private void Start()
     {
-        goal = this.transform.position;
+        rigidbody = GetComponent<Rigidbody>();
+        goal = transform.position;
     }
 
     private void Update()
@@ -18,15 +24,28 @@ public class Move : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit) && Input.GetMouseButtonDown(0))
         {
+            jumped = false;
             goal = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-            transform.LookAt(goal);
+            //Debug.Log("Current position vector: " + goal.ToString());
+        }
 
-            if(Vector3.Distance(transform.position, goal) > accuracy)
+        Vector3 direction = goal - transform.position;
+
+        if (Vector3.Distance(transform.position, goal) > accuracy)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                Quaternion.LookRotation(direction),
+                Time.deltaTime * rotSpeed);
+
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        else
+        {
+            if(!jumped)
             {
-                transform.Translate(0, 0, speed * Time.deltaTime);
+                rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                jumped = true;
             }
-
-            Debug.Log("Current position vector: " + goal.ToString());
         }
     }
 }
