81286a7cd33a3ebd121356ac532fcceaf57d2b01
Charlie Phillips <hi.charliephillips@gmail.com>
2021-02-28 21:25:43 -0600

    point/click pathfinding

---
+++ Chapter2_MovementActivity/Assets/Scripts/AgentController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                this.GetComponent<NavMeshAgent>().SetDestination(hit.point);
+            }
+        }
+    }
+}
