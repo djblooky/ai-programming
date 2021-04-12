f2855d7d41947d5fa3fbb2ee39cf1d8abd36a949
Charlie Phillips <hi.charliephillips@gmail.com>
2021-04-11 21:44:06 -0500

    tutorial 2 WIP

---
+++ Week11_ProceduralLandmassGeneration/Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        DrawDefaultInspector();
+
+    }
+}
---
+++ Week11_ProceduralLandmassGeneration/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth, mapHeight;
+    public float noiseScale;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
