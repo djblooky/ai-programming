f3e1a80b981bdd1788ff1c6cb5b8304c6d84e845
Charlie Phillips <hi.charliephillips@gmail.com>
2021-04-11 23:21:16 -0500

    completed video 3

--- Week11_ProceduralLandmassGeneration/Assets/Editor/MapGeneratorEditor.cs
+++ Week11_ProceduralLandmassGeneration/Assets/Editor/MapGeneratorEditor.cs
@@ -1,12 +1,24 @@
 ﻿using UnityEditor;
+using UnityEngine;
 
+[CustomEditor(typeof(MapGenerator))]
 public class MapGeneratorEditor : Editor
 {
     public override void OnInspectorGUI()
     {
         MapGenerator mapGen = (MapGenerator)target;
 
-        DrawDefaultInspector();
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
 
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
     }
 }
--- Week11_ProceduralLandmassGeneration/Assets/Scripts/MapGenerator.cs
+++ Week11_ProceduralLandmassGeneration/Assets/Scripts/MapGenerator.cs
@@ -4,12 +4,43 @@ public class MapGenerator : MonoBehaviour
 {
     public int mapWidth, mapHeight;
     public float noiseScale;
+    public bool autoUpdate;
 
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+
+    private void OnValidate()
+    {
+        if(mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+
+        if(mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+
+        if(lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+
+        if(octaves < 0)
+        {
+            octaves = 0;
+        }
     }
 }
